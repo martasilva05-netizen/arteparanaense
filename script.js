bc1e4b774be65c4cecd7146266fdc9328c808a03
martasilva05-netizen <marta.silva05@escola.pr.gov.br>
2026-09-16 17:34:57 -0300

    atualizei meu codígo

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const botao = document. querySelector("#botaomensagem");
-cons mensagem = document. querySelector("#mensageminterativa");
-botao. addEvetListener("clik", mostrarmesagem);
-function mostrarMensagem() {
-    Mensagem.textContent =
-    "vocẽ acabou de criar uma interaçãocom javaScript";
-}
+const botoesCurtir = document. querySelectorAll(".curtir");
+botoesCurtir. forEach(function(botaoCurtir){
+    let curtir = false;
+    botaoCurtir.addEventListener("click", curtir);
+    function curtir(){
+        const contador = botaoCurtir. querySelector("span");
+        if(curtir===false){
+            contador.textContent++;
+            curtir = true; }
+            alse{
+                contador.textContent--;
+                curtir = false;
+            }
+    }
+});
